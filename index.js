47abc71f27ad43d63e523ef5195ed33e159f991e
Voloshyn Mykyta <voloshyn.nekit34@gmail.com>
2023-05-30 21:33:44 +0300

    homework-switch

--- index.js
+++ index.js
@@ -1,50 +1,22 @@
-// let num1 = Number(prompt('Введіть перше число: '));
-// let num2 = Number(prompt('Введіть друге число: '));
-
-// function addTwoNumbers(num1, num2) {
-//     const result = num1 + num2;
-//     return result;
-// }
-
-// let result = addTwoNumbers(num1, num2);
-// console.log(result);
-
-// zadacha1
-
-function numberUp(num1, num2) {
-    let result;
-    if (num1 > num2) {
-        result = num1;
+switch (drink = Number(prompt('Вибери напій: 1 - Сік, 2 - Вода, 3 - Кава, 4 - Чай, 5 - Лимонад'))) {
+    case 1: {
+        console.log('Ви вибрали сік');
+        break
     }
-    else if (num2 > num1) {
-        result = num2;
+    case 2: {
+        console.log('Ви вибрали воду');
+        break
     }
-    return result;
-}
-
-let result = numberUp(25, 10);
-console.log(result);
-
-
-// zadacha2
-
-function srArefmet (num3, num4) {
-    const result1 = (num3 + num4) / 2;
-    return result1;
-}
-
-let result1 = srArefmet(7, 8);
-console.log(result1);
-
-//zadacha3
-
-function parneOrNeparne (num5) {
-    let result3;
-    if (num5 % 2 === 0) {
-        result3 = console.log('парне');
-    } else if (num5 % 2 !== 0) {
-        result3 = console.log('непарне');
+    case 3: {
+        console.log('Ви вибрали каву');
+        break
+    }
+    case 4: {
+        console.log('Ви вибрали чай');
+        break
+    }
+    case 5: {
+        console.log('Ви вибрали лимонад');
+        break
     }
 }
-
-let result3 = parneOrNeparne (7)
