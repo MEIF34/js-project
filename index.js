d493440c742c5fa80542cd090c3b25894bed883a
Voloshyn Mykyta <voloshyn.nekit34@gmail.com>
2023-05-31 17:34:00 +0300

    dz-hof

--- index.js
+++ index.js
@@ -1,22 +1,75 @@
-switch (drink = Number(prompt('Вибери напій: 1 - Сік, 2 - Вода, 3 - Кава, 4 - Чай, 5 - Лимонад'))) {
-    case 1: {
-        console.log('Ви вибрали сік');
-        break
-    }
-    case 2: {
-        console.log('Ви вибрали воду');
-        break
-    }
-    case 3: {
-        console.log('Ви вибрали каву');
-        break
-    }
-    case 4: {
-        console.log('Ви вибрали чай');
-        break
-    }
-    case 5: {
-        console.log('Ви вибрали лимонад');
-        break
+function washingFace() {
+    console.log('Washing the face');
+}
+
+function breakfast() {
+    console.log('Breakfast');
+} 
+
+function shower() {
+    console.log('Shower');
+}
+
+function brushingTeeth() {
+    console.log('Brushing teeth');
+}
+
+function goToWork() {
+    console.log('Go to work');
+}
+
+function lunch() {
+    console.log('Lunch');
+}
+
+function goToGym() {
+    console.log('Go to gym');
+}
+
+function goToHome() {
+    console.log('Go home');
+}
+
+function dinner() {
+    console.log('Dinner');
+}
+
+function educationJS() {
+    console.log('Education JS');
+}
+
+function sleep() {
+    console.log('Sleep');
+}
+  
+function todo(timeOfDay) { // Higher-order function
+    switch(timeOfDay) {
+        case 'morning': {
+            washingFace();
+            breakfast();
+            shower();
+            brushingTeeth();
+            goToWork();
+            break;
+        }
+        case 'lunch': {
+            lunch();
+            goToGym();
+            goToHome();
+            break;
+        }
+        case 'evening': { 
+            dinner();
+            educationJS();
+            sleep();
+            break;
+        }
+        default: {
+            console.log('Error! Enter a valid time of day!');
+        } 
     }
 }
+  
+todo('morning');
+todo('lunch');
+todo('evening');
