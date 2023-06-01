66598677e03ebbab695d30143888541d7c176b41
Voloshyn Mykyta <voloshyn.nekit34@gmail.com>
2023-06-01 20:33:05 +0300

    object-bookstore

--- index.js
+++ index.js
@@ -1,13 +1,46 @@
-function Cat(name, color, breed, age) {
+function Book(name, author, year, price) {
     this.name = name;
-    this.color = color;
-    this.breed = breed;
-    this.age = age;
-    this.run = function() {
-        return this.name + ' is running';
+    this.author = author;
+    this.year = year;
+    this.price = price;
+    
+    this.getName = function() {
+        return this.name;
+    }
+   
+    this.getAuthor = function() {
+        return this.author
+    } 
+
+    this.getYear = function() {
+        return this.year;
+    }
+
+    this.getPrice = function() {
+        return this.price;
+    }
+
+    this.setName = function(newName) {
+        this.name = newName;
+    }
+
+    this.setAuthor = function(newAuthor) {
+        this.author = newAuthor;
+    }
+
+    this.setYear = function(newYear) {
+        this.year = newYear;
+    }
+
+    this.setPrice = function(newPrice) {
+        this.price = newPrice;
+    }
+
+    this.calcSalePrice = function(salePercentation) {
+        const salePrice = this.price - (this.price * (salePercentation / 100));
+        return salePrice.toFixed(2);
     }
 }
 
-const cat1 = new Cat('Meif', 'black', 'Mei-Kun', 3);
-const cat2 = new Cat('Roxy', 'pink', 'sexy', 3);
-const cat3 = new Cat('Debik', 'blue', 'gay');
+const book1 = new Book('Name1', 'Author1', 0, 1000.99);
+const book2 = new Book('Lord of the Rings', 'Golum', 2000, 9.99);
