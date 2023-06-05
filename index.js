1594e472f15a2c7248bbfcace0155ece3c96906f
Voloshyn Mykyta <voloshyn.nekit34@gmail.com>
2023-06-05 17:55:26 +0300

    tasks-function+obj

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*
+
 const user = {
     name: 'John',
     surname: 'Doe',
@@ -11,3 +13,48 @@ console.log(`${user.name} ${user.surname} ${user.age} ${user.favoriteColor} ${us
 for (let key in user) {
     console.log(`${key} --- ${user[key]}`);
 }
+
+*/
+
+//task1
+
+/*
+
+function znachObj(obj) {
+    for (let key in obj) {
+        console.log(`'${key}' : **${obj[key]}**`);
+    }
+}
+
+const obj = {
+    name: 'John',
+    surname: 'Doe',
+    age: 35,
+    favoriteColor: 'red',
+    avatar: 'https://...'
+}
+
+*/
+
+//task2
+
+const departmentSalary = {
+    HR: 20000,
+    development: 1000000,
+    drive: 300,
+    assistant: 5000,
+    marketing: undefined,
+}
+
+function srZnachZp(departmentSalary) {
+    let result = 0;
+    
+    for (let key in departmentSalary) {
+        if (typeof departmentSalary[key] === 'number') {
+            result = result + departmentSalary[key];
+        }
+    }
+
+    return result;
+}
+
