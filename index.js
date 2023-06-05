90bb2f08a7d67505e5d465cea55cb530856a268a
Voloshyn Mykyta <voloshyn.nekit34@gmail.com>
2023-06-05 19:38:35 +0300

    array-zadachi

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+/*
+
 const userTel1 = '380931111111';
 const userTel2 = '380932222222';
 const userTel3 = '380933333333';
@@ -17,6 +19,8 @@ const arr = [2, 4, 6, 7]
 const arr2 = new Array(1, 2, 4, 'Oleg');
 
 //
+
+*/
 
 /*
 
@@ -50,6 +54,8 @@ for(let i = 0; i < array.length; i++) {
 
 //task2 
 
+/*
+
 const array2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
 for(let i = 0; i < array2.length; i++) {
@@ -58,3 +64,34 @@ for(let i = 0; i < array2.length; i++) {
     }
 }
 
+*/
+
+/*
+
+const array = [3, 6, 8, 2, 3, 5, 1];
+
+function sumArray(array) {
+    let result = 0;
+
+    for(let i = 0; i < array.length; i++) {
+        result = result + array[i];
+    }
+
+    return result;
+}
+
+*/
+
+const array = [3, 6, 8, 2, 3, 5, 1];
+
+function largeNumber(array) {
+    let max = array[0];
+
+    for(let i = 0; i < array.length; i++) {
+        if (array[i] > max) {
+            max = array[i];
+        }       
+    }
+
+    return max;
+}
