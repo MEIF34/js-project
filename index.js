fa0d3d14e63530812fb38f8f8dbf24cc34784359
Voloshyn Mykyta <voloshyn.nekit34@gmail.com>
2023-06-07 19:01:16 +0300

    fullname = name + lastName

--- index.js
+++ index.js
@@ -1,53 +1,44 @@
-//task1 
+const users = [
+    {
+    name: 'John',
+    lastName: 'Doe',
+    age: 18,
+    email: 'meil@com'
+    },
+    {
+    name: 'Jane',
+    lastName: 'Doe',
+    age: 20,
+    email: 'meil@com'
+    },
+    {
+    name: 'Josh',
+    lastName: 'Doe',
+    age: 17,
+    email: 'meil@com'
+    },
+    {
+    name: 'Jake',
+    lastName: 'Doe',
+    age: 21,
+    email: 'meil@com'
+    },
+    {
+    name: 'Jackson',
+    lastName: 'Doe',
+    age: 17,
+    email: 'meil@com'
+    },
+]
 
-const arr1 = [1, 2, 3];
-const arr2 = [4, 5, 6];
-const arr3 = arr1.concat(arr2);
+function newUserObject(user) {
+    return {
+        fullname: `${user.name} ${user.lastName}`,
+        age: user.age,
+        email: user.email
+    }
+}
 
-console.log(arr3);
-
-//task2
-
-const arr4 = arr1.reverse(); // from the first task
-
-console.log(arr4);
-
-//task3
-
-const arr5 = [3, 4, 5];
-
-arr5.push(4, 8, 10);
-
-console.log(arr5);
-
-//task4
-
-const strArr = ['aaa', 'bbb', 'ccc'];
-
-console.log(strArr.pop());
-
-//task5 
-
-const arr6 = ['3', 7, 10];
-
-arr6.unshift(2, 8);
-
-console.log(arr6);
-
-//task6
-
-const arr7 = ['3', 7, 10];
-
-arr8 = arr7.toString();
-
-console.log(arr8);
-
-//forEach
-
-const forEachArr = [2, 44, 11, 234, 8, 2, 4, 1];
-
-// forEachArr.forEach(Element => console.log(Element + 100));
-
-forEachArr.forEach((item, index, array) => {
-    console.log(item + 100);
+const newUsersArray = users.map((item) => {
+    return newUserObject(item);
 })
