cbb6283ce4fe14add03229ad7f7efca760775144
Voloshyn Mykyta <voloshyn.nekit34@gmail.com>
2023-07-09 21:29:59 +0300

    Spread operator

--- index.js
+++ index.js
@@ -5,27 +5,14 @@
 // REST - ...
 // ..., ..., ..., ..., -> []
 
-// Задача: знайти суму двох елементів, а всі "зайві " аргументи покласти до массиву
-function sum (a, b, ...arrayOfRestArguments) {
-    console.log(arrayOfRestArguments);
-    return a + b;
-}
-
-sum (3, 4); // 7
-console.log(sum (1, 2, 3, 4, 5, 6)); // 3
-
-/*
-
-const arrowSum = (...rest) => {
-    let sum = 0;
-
-    for (let i = 0; i < rest.length; i++) {
-        sum += rest[i];
-    }
-
-    return sum;
-} 
-
-*/
-
 const arrowSumReduce = (...restArray) => restArray.reduce((accumulator, current) => accumulator + current, 0);
+
+// SPREAD
+// [] -> ..., ..., ..., ...,
+
+const numbers = [10, 2, 3, 4, 5];
+
+console.log(arrowSumReduce(numbers));
+console.log(arrowSumReduce(numbers[0], numbers[1], numbers[2], numbers[3], numbers[4]));
+console.log(arrowSumReduce(...numbers));
+console.log(Math.min(...numbers));
